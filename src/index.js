4f718da0e75b5c9044face6d66c10237cad85101
Franco Mendez <125481413+francomendezok@users.noreply.github.com>
2023-09-26 12:36:30 -0300

    Header Section

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-import someConfig from "some-other-config-you-use";
-import eslintConfigPrettier from "eslint-config-prettier";
+ 
+//   async function getUserLocation() {
+//     try {
+//       const response = await fetch('https://ipinfo.io?token=c754c5e436b57a');
+//       const data = await response.json();
+//       return data;
+//     } catch (error) {
+//       console.error('Error al obtener la ubicación del usuario:', error);
+//     }
+//   }
 
-export default [
-  someConfig,
-  eslintConfigPrettier,
-];
+//   async function getWeather () {
+//     try {
+//         const city = await getUserLocation();
+//         if (city) {
+//             const url = "https://api.weatherapi.com/v1/current.json?key=6c38787677004652bda203136232209&q=" + city.city
+//             const weather = await fetch(url)
+//             const log = await weather.json();
+//             console.log(log);
+//         }
+//     } catch (error) {
+//         console.log(error);
+//     }
+//   }
+  
+
+// getWeather();
